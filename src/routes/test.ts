fccb097d081937e357cbbba0daf419f37dc04d18
raine-works <mail@raineworks.com>
2024-10-09 23:17:38 +0000

    cleaning up

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,9 +1,9 @@
-import { Hono } from 'jsr:@hono/hono';
+import { Hono } from 'jsr:@hono/hono'
 
-const app = new Hono();
+const app = new Hono()
 
 app.get('/', (c) => {
-    return c.json({ msg: 'This is a test' });
-});
+	return c.json({ msg: 'This is a test' })
+})
 
-export default app;
+export default app
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,7 @@
-import { z } from 'npm:zod';
+import { z } from 'npm:zod'
 
 const EnvSchema = z.object({
-    PORT: z.string(),
-});
+	PORT: z.string(),
+})
 
-export const env = EnvSchema.parse(Deno.env.toObject());
+export const env = EnvSchema.parse(Deno.env.toObject())
